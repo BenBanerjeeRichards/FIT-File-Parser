d790bf26235aabee80545396295aa3de26ee0fb4
Ben Banerjee-Richards <dev@benbr.com>
2016-05-14 17:10:46 +0100

    Added field name

--- src/com/benbr/parser/types/FieldDefinition.groovy
+++ src/com/benbr/parser/types/FieldDefinition.groovy
@@ -8,9 +8,24 @@ class FieldDefinition {
         this.type = type
     }
 
+    FieldDefinition(int definitionNumber, int size, int type, String name) {
+        this.definitionNumber = definitionNumber
+        this.size = size
+        this.type = type
+        this.name = name
+    }
     private int definitionNumber;
     private int size;
     private int type;
+    String name;        // From Profiles.xlxs.
+
+    String getName() {
+        return name
+    }
+
+    void setName(String name) {
+        this.name = name
+    }
 
     int getDefinitionNumber() {
         return definitionNumber
