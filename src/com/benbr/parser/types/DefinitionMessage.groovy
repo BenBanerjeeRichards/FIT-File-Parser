666e602bdb5480093cb91024c5793552d81eceaf
Ben Banerjee-Richards <dev@benbr.com>
2016-05-14 18:28:17 +0100

    Fixed type

--- src/com/benbr/parser/types/DefinitionMessage.groovy
+++ src/com/benbr/parser/types/DefinitionMessage.groovy
@@ -9,23 +9,23 @@ class DefinitionMessage {
     }
 
     DefinitionMessage(int reserverd, ArchitectureType architectureType, int globalMessageNumber, int fieldCount, List<FieldDefinition> fieldDefinitions, List<FieldDefinition> developerFieldDefinitions) {
-        this.reserverd = reserverd
+        this.reserved = reserverd
         this.architectureType = architectureType
         this.globalMessageNumber = globalMessageNumber
         this.fieldDefinitions = fieldDefinitions
         this.developerFieldDefinitions = developerFieldDefinitions
     }
 
-    private int reserverd
+    private int reserved
     private ArchitectureType architectureType
     private int globalMessageNumber
     private List<FieldDefinition> fieldDefinitions
-    
+
     // Usually not set (depends of reserved bit in header)
     private List<FieldDefinition> developerFieldDefinitions;
 
     int getReserverd() {
-        return reserverd
+        return reserved
     }
 
     ArchitectureType getArchitectureType() {
