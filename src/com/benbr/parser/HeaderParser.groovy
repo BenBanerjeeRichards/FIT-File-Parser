7f7a4c99d66c6f0b31e16ed248c17c80367d73b6
Ben Banerjee-Richards <dev@benbr.com>
2016-05-14 16:03:33 +0100

    Use Data Input Stream for parsing

--- src/com/benbr/parser/HeaderParser.groovy
+++ src/com/benbr/parser/HeaderParser.groovy
@@ -1,18 +1,22 @@
 package com.benbr.parser
 
+import com.benbr.Util
+
 class HeaderParser {
 
-    public Header parseHeader(List<Integer> bytes) {
-        int headerSize = bytes[0]
-        int protocolVersion = bytes[1]
-        int profileVersion = (bytes[3] << 8 | bytes[2])
-        int dataSize = bytes[7] << 24 | bytes[6] << 16 | bytes[5] << 8 | bytes[4]
+    public Header parseHeader(DataInputStream byteStream) {
+        int headerSize = byteStream.read()
+        int[] bytes = Util.readUnsignedValues(byteStream, headerSize - 1)
+
+        int protocolVersion = bytes[0]
+        int profileVersion = (bytes[2] << 8 | bytes[1])
+        int dataSize = bytes[6] << 24 | bytes[5] << 16 | bytes[4] << 8 | bytes[3]
         char[] dataType = new char[4]
-        dataType[0] = bytes[8]
-        dataType[1] = bytes[9]
-        dataType[2] = bytes[10]
-        dataType[3] = bytes[11]
-        int crc = bytes[13] << 8 | bytes[12]
+        dataType[0] = bytes[7]
+        dataType[1] = bytes[8]
+        dataType[2] = bytes[9]
+        dataType[3] = bytes[10]
+        int crc = bytes[12] << 8 | bytes[11]
 
         return new Header(headerSize, protocolVersion, profileVersion, dataSize, dataType, crc)
     }
