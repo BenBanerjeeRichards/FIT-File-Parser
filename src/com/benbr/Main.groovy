fd7596d36ae88e4b73dbfc02c046d9294b2b95d7
Ben Banerjee-Richards <dev@benbr.com>
2016-05-15 12:53:38 +0100

    (Messy) debug code to find dynamic fields - just to investigate some things before implementing them.

--- src/com/benbr/Main.groovy
+++ src/com/benbr/Main.groovy
@@ -2,13 +2,34 @@ package com.benbr
 
 import com.benbr.parser.FitParser
 import com.benbr.profile.CSVProfileParser
+import com.benbr.profile.Field
 import com.benbr.profile.XLSXProfileParser
 
 
 class Main {
 
     public static void main(String[] args) {
-        new FitParser(new File("fit.fit"))
+        //new FitParser(new File("fit.fit"))
+        def profile = new CSVProfileParser(new File("profile.csv")).getFields()
+        profile.eachWithIndex{ Map.Entry<String, List<Field>> entry, int i ->
+            def prev;
+            boolean mentionedSection = false;
+
+            entry.value.each {
+                if (it.definitionNumber == null && !mentionedSection && it.name.trim().size() > 0) {
+                    mentionedSection = true
+                    println "$entry.key: $prev.name"
+                }
+
+                if (prev != null) {
+                    if (prev.definitionNumber == null && it.definitionNumber != null) {
+                        mentionedSection = false;
+                    }
+                }
+
+                prev = it
+            }
+        }
     }
 
 }
