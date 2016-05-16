ddd5df606c13f36d9397a851cee7e46b50ee734b
Ben Banerjee-Richards <dev@benbr.com>
2016-05-16 16:01:25 +0100

    Type for storing data messages.
    Uses maps and groovy propertyMissing to provide the impression of introspection when accessing fields.

--- src/com/benbr/parser/DataMessage.groovy
+++ src/com/benbr/parser/DataMessage.groovy
@@ -1,4 +1,16 @@
 package com.benbr.parser
 
 class DataMessage {
+
+    def fields = [:]
+
+    def propertyMissing(String name, String value) {
+        fields[name] = value
+    }
+
+    def propertyMissing(String name) {
+        return fields[name]
+    }
+
+
 }
