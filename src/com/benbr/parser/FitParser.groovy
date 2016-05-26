cf0a30394254bd7431a09f1244578bd7b59f9437
Ben Banerjee-Richards <dev@benbr.com>
2016-05-26 20:38:49 +0100

    Refactored.

--- src/com/benbr/parser/FitParser.groovy
+++ src/com/benbr/parser/FitParser.groovy
@@ -1,26 +1,34 @@
 package com.benbr.parser
 
 import com.benbr.parser.types.DefinitionMessage
-import com.benbr.parser.types.FieldDefinition
 import com.benbr.parser.types.MessageType
 import com.benbr.profile.CSVProfileParser
 import com.benbr.profile.CSVTypeParser
-import com.benbr.profile.Constants
+import com.benbr.profile.types.EnumerationType
+import com.benbr.profile.types.ProfileField
+import org.apache.poi.hdgf.streams.Stream
+
+import javax.xml.crypto.Data
+import java.util.concurrent.LinkedBlockingDeque
+import java.util.concurrent.LinkedBlockingQueue
 
 class FitParser {
 
-    private DataInputStream fitStream;
+    private HashMap<String, List<ProfileField>> profile;
+    private HashMap<String, EnumerationType> types;
 
-    public FitParser(File fitFile) {
-        fitStream = new DataInputStream(new FileInputStream(fitFile))
+    public FitParser() {
+        profile = new CSVProfileParser(new File("profile.csv")).getFields()
+        types = new CSVTypeParser(new File("types.csv")).parse()
+    }
+
+    private void parseFile(File fitFile, Queue<DataMessage> messageQueue) {
+        DataInputStream fitStream = new DataInputStream(new FileInputStream(fitFile))
         def locals = new HashMap<Integer, DefinitionMessage>()
-        def profile = new CSVProfileParser(new File("profile.csv")).getFields()
-        def types = new CSVTypeParser(new File("types.csv")).parse()
         new FileHeaderParser().parseHeader(fitStream)
 
         while (fitStream.available() > 0) {
             def header = new MessageHeaderParser().parse(fitStream.read())
-            println header
 
             if (header.messageType == MessageType.DEFINITION) {
                 DefinitionMessage message = new DefinitionMessageParser().parse(fitStream, header)
@@ -28,9 +36,28 @@ class FitParser {
                 DefinitionMessageParser.associateFieldDefinitionWithGlobalProfile(profile, message, message.getGlobalMessageNumber())
             } else {
                 DataMessage message = new DataMessageParser(profile, types).parse(fitStream, header, locals)
+                messageQueue.add(message)
             }
         }
-
     }
+
+    public List<DataMessage> parse(File fitFile) {
+        Queue<DataMessage> messageQueue = new LinkedBlockingQueue<>()
+        parseFile(fitFile, messageQueue)
+        return messageQueue.toList()
+    }
+
+    public Queue<DataMessage> parseAsync(File fitFile) {
+        Queue<DataMessage> messageQueue = new LinkedBlockingQueue<>()
+        new Thread(new Runnable() {
+            @Override
+            void run() {
+                parseFile(fitFile, messageQueue)
+            }
+        }).start()
+
+        return messageQueue
+    }
+
 
 }
