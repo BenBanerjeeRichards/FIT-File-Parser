ea8c11444ba16a95241ac657b8f4c77932b6a5b5
Ben Banerjee-Richards <dev@benbr.com>
2016-05-30 16:01:45 +0100

    Example usage

--- src/com/benbr/Main.groovy
+++ src/com/benbr/Main.groovy
@@ -5,7 +5,15 @@ import com.benbr.parser.FitParser
 class Main {
 
     public static void main(String[] args) {
-        def parser = new FitParser(new File("fit.fit"))
+
+        new FitParser().parse(new File("fit/fit.fit")).each { message ->
+            println message.getType()
+            message.fields.each{field->
+                println "\t ${field.getKey()} : ${field.getValue()} ${message.fieldDefinitions[field.getKey()]?.getUnit()}"
+            }
+
+            println ""
+        }
     }
 
 }
