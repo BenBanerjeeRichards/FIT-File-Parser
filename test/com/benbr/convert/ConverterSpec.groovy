4fb7eb91cd61469d8aef2fd8541e82149b1cbfe2
Ben Banerjee-Richards <dev@benbr.com>
2016-05-30 19:15:56 +0100

    Added more types and fixed some spelling issues.

--- test/com/benbr/convert/ConverterSpec.groovy
+++ test/com/benbr/convert/ConverterSpec.groovy
@@ -9,12 +9,12 @@ class ConverterSpec extends Specification {
 
     def "Given some converters, conversion works correctly in both directions"() {
         when:
-        def c= new Conversion().to(Unit.FARENHEIGHT).from(Unit.CELCIUS).constants(1.8, 32)
+        def c= new Conversion().to(Unit.FAHRENHEIT).from(Unit.CELSIUS).constants(1.8, 32)
         Converter.addConversion(c)
 
         then:
-        Converter.convert(5, Unit.CELCIUS, Unit.FARENHEIGHT) == 41d
-        Converter.convert(41, Unit.FARENHEIGHT, Unit.CELCIUS) == 5d
+        Converter.convert(5, Unit.CELSIUS, Unit.FAHRENHEIT) == 41d
+        Converter.convert(41, Unit.FAHRENHEIT, Unit.CELSIUS) == 5d
     }
 
 }
