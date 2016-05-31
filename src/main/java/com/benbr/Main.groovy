bcbb924bed20065d162473eb18403a22eea415fa
Ben Banerjee-Richards <dev@benbr.com>
2016-05-31 19:58:56 +0100

    Updated README

--- src/main/java/com/benbr/Main.groovy
+++ src/main/java/com/benbr/Main.groovy
@@ -5,6 +5,7 @@ import main.java.com.benbr.converter.Converter
 import main.java.com.benbr.converter.MessageConverter
 import main.java.com.benbr.converter.Unit
 import main.java.com.benbr.parser.DataMessage
+import main.java.com.benbr.parser.DataMessageParser
 import main.java.com.benbr.parser.FitParser
 import main.java.com.benbr.profile.Constants
 
@@ -14,7 +15,7 @@ class Main {
         Constants.populateUnitToSymbol()
         Converter.loadConversions()
 
-        HashMap<Unit, Unit> conversionPolicy = [
+        HashMap<Unit, Unit> globalConversionPolicy = [
                 (Unit.SEMICIRCLE) : Unit.DEGREE,
                 (Unit.METRE) : Unit.MILE,
         ]
@@ -24,18 +25,33 @@ class Main {
         ]
 
 
-        def converter = new MessageConverter(new ConversionPolicy(fieldPolicy, conversionPolicy))
+        def converter = new MessageConverter(new ConversionPolicy(fieldPolicy, globalConversionPolicy))
 
         new FitParser().parse(new File("fit/fit.fit")).each { message ->
             println message.getType()
             DataMessage converted = converter.convertMessage(message)
 
             converted.fields.each{ field ->
-                println "\t${field.getKey()}: ${field.getValue()} (${converted.unitSymbols[field.getKey()]})"
+                println "\t ${field.getKey()}: ${field.getValue()} (${converted.unitSymbols[field.getKey()]})"
             }
-
-            println ""
         }
+
+
+//        def parser = new FitParser()
+//        List<DataMessage> messages = parser.parse(new File("fit/fit.fit"))
+//
+//        // Iterate over each of the data messages in the FIT file
+//        messages.each {message ->
+//            // Print the data message name (for example, 'record' or 'file_id')
+//            println message.getType()
+//
+//            // Iterate over the fields
+//            message.fields.each { field ->
+//                // Look up the unit symbol in the hashmap
+//                String unitSymbol = message.unitSymbols[field.getKey()]
+//                println "\t${field.getKey()} : ${field.getValue()} (${unitSymbol})"
+//            }
+//        }
     }
 
 }
